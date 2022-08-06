f86365c1d84f210a3a66b11b636d65502c7d4625
amesemyta1 <amesemyta@gmail.com>
2022-08-06 17:56:19 +0300

    ls3 tsk3

---
+++ lesson3/task3/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Comment from './Comment.jsx';
+
+const userInfo = {
+  name: 'Tom',
+  avatarUrl: 'https://avatars3.githubusercontent.com/u10001',
+};
+
+const App = () => {
+  return <Comment user={userInfo} text="Good job" date={new Date()} />;
+};
+
+export default App;
