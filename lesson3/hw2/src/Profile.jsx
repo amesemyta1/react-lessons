208612c3b77ebadd1e5eb0fb48773361f4895061
amesemyta1 <amesemyta@gmail.com>
2022-08-07 16:16:50 +0300

    Profile

--- lesson3/hw2/src/Profile.jsx
+++ lesson3/hw2/src/Profile.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import Moment from 'react-moment';
 
-const calculateAge = birthday => {
-  const ageDifMs = Date.now() - birthday.getTime();
-  const ageDate = new Date(ageDifMs);
-  return Math.abs(ageDate.getUTCFullYear() - 1970);
-};
-
 const Profile = props => {
   return (
     <div>
-      <p className="profile__name">
+      <div className="profile__name">
         {props.firstName} {props.lastName}
-      </p>
-      <p className="profile__birth">
+      </div>
+      <div className="profile__birth">
         Was born <Moment format="DD MMM YY">{props.birthDate}</Moment> in {props.birthPlace}
-      </p>
+      </div>
     </div>
   );
 };
