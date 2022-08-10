1888536fdb321f4e003ba21b4eb439903d144fb7
amesemyta1 <amesemyta@gmail.com>
2022-08-10 17:54:10 +0300

    hw2

--- lesson5/hw2/src/ColorPicker.jsx
+++ lesson5/hw2/src/ColorPicker.jsx
@@ -4,19 +4,37 @@ class ColorPicker extends Component {
   constructor(props) {
     super(props);
 
-    this.colorText = {
+    this.state = {
       text: '',
     };
   }
 
+  changeColorTitle = color => {
+    this.setState({
+      text: color,
+    });
+  };
+
   render() {
     return (
       <div>
-        <div className="picker__title">{this.colorText}</div>
+        <div className="picker__title">{this.state.text}</div>
         <div>
-          <button className="picker__button picker__button_coral"></button>
-          <button className="picker__button picker__button_aqua"></button>
-          <button className="picker__button picker__button_bisque"></button>
+          <button
+            className="picker__button picker__button_coral"
+            onMouseEnter={() => this.changeColorTitle('Coral')}
+            onMouseLeave={() => this.changeColorTitle('')}
+          ></button>
+          <button
+            className="picker__button picker__button_aqua"
+            onMouseEnter={() => this.changeColorTitle('Aqua')}
+            onMouseLeave={() => this.changeColorTitle('')}
+          ></button>
+          <button
+            className="picker__button picker__button_bisque"
+            onMouseEnter={() => this.changeColorTitle('Bisque')}
+            onMouseLeave={() => this.changeColorTitle('')}
+          ></button>
         </div>
       </div>
     );
