5d69b6765eba49ae9d3be1f92aab6d4a0ee1aa90
amesemyta1 <amesemyta@gmail.com>
2022-08-11 19:08:01 +0300

    tsk1

--- lesson6/task1/src/Auth.jsx
+++ lesson6/task1/src/Auth.jsx
@@ -4,11 +4,11 @@ import Login from './Login.jsx';
 import Logout from './Logout.jsx';
 
 class Auth extends Component {
-  constructor(props) {
-    super(props);
+  constructor({ isLoggedIn }) {
+    super({ isLoggedIn });
 
     this.state = {
-      isLoggedIn: props.isLoggedIn,
+      isLoggedIn,
     };
   }
 
