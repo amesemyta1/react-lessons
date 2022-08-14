0099c87bd3b39ef4944c916cc9458adfd1cdeb10
amesemyta1 <amesemyta@gmail.com>
2022-08-14 19:37:11 +0300

    hw1

---
+++ lesson8/hw1/src/ConnectionStatus.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class ConnectionStatus extends Component {
+  state = {
+    onlineStatus: 'online',
+  };
+
+  componentDidMount() {
+    window.addEventListener('online', this.updateOnlineStatus);
+    window.addEventListener('offline', this.updateOnlineStatus);
+  }
+
+  updateOnlineStatus = e => {
+    if (e.type === 'offline') {
+      this.setState({
+        onlineStatus: 'offline',
+      });
+    }
+    if (e.type === 'online') {
+      this.setState({
+        onlineStatus: 'online',
+      });
+    }
+  };
+
+  render() {
+    const status = this.state.onlineStatus;
+    return status === 'online' ? (
+      <div className="status">{this.state.onlineStatus}</div>
+    ) : (
+      <div className="status status_offline">{this.state.onlineStatus}</div>
+    );
+  }
+}
+
+export default ConnectionStatus;
