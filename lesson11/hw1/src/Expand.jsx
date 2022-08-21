ad4e45d9b2e2e1b6491434eec6d4f1684ba48ceb
amesemyta1 <amesemyta@gmail.com>
2022-08-21 17:22:49 +0300

    hw1

--- lesson11/hw1/src/Expand.jsx
+++ lesson11/hw1/src/Expand.jsx
@@ -26,18 +26,18 @@ class Expand extends Component {
       <i class="fas fa-chevron-up"></i>
     );
 
-    const btnState = !this.state.isOpen ? this.showDialog : this.hideDialog;
-    const contentState = !this.state.isOpen ? '' : this.props.children;
+    const handleButton = !this.state.isOpen ? this.showDialog : this.hideDialog;
+    const handleContent = !this.state.isOpen ? null : this.props.children;
 
     return (
       <div class="expand border">
         <div class="expand__header">
           <span class="expand__title">{this.props.title}</span>
-          <button class="expand__toggle-btn" onClick={btnState}>
+          <button class="expand__toggle-btn" onClick={handleButton}>
             {icon}
           </button>
         </div>
-        <div class="expand__content">{contentState}</div>
+        <div class="expand__content">{handleContent}</div>
       </div>
     );
   }
