f6ae5716df84343a187af4fdbd160ccf15dcd499
amesemyta1 <amesemyta@gmail.com>
2022-08-29 22:40:15 +0300

    ls13, task

---
+++ lesson13/task1/src/App.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable class-methods-use-this */
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Products from './Products';
+import Contacts from './Contacts';
+import PageNotFound from './PageNotFound';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="page">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/products">
+              <Products />
+            </Route>
+            <Route path="/contacts">
+              <Contacts />
+            </Route>
+            <Route path="*">
+              <PageNotFound />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default App;
