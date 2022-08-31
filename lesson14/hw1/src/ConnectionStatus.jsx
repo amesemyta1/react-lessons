2c8c18ceadc8f8e43b6316638b3b4720ce1da48b
amesemyta1 <amesemyta@gmail.com>
2022-08-31 22:48:46 +0300

    hw1

---
+++ lesson14/hw1/src/ConnectionStatus.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+
+const ConnectionStatus = () => {
+  const [status, seSstatus] = useState(true);
+
+  useEffect(() => {
+    const { onLine } = window.navigator;
+    seSstatus({ connection: onLine });
+    const handleConnection = e => {
+      const { onLine } = e.target;
+      seSstatus({ connection: onLine });
+    };
+    window.addEventListener('online', handleConnection);
+    window.addEventListener('offline', handleConnection);
+
+    return () => {
+      window.removeEventListener('online', handleConnection);
+      window.removeEventListener('offline', handleConnection);
+    };
+  });
+
+  const { connection } = status;
+
+  return connection ? (
+    <div className="status">online</div>
+  ) : (
+    <div className="status status_offline">offline</div>
+  );
+};
+
+export default ConnectionStatus;
