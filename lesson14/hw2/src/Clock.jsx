551f51508854900a8db73ca4886b6def5471be0f
amesemyta1 <amesemyta@gmail.com>
2022-09-01 01:20:44 +0300

    hw2

---
+++ lesson14/hw2/src/Clock.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import './clock.scss';
+
+const getTimeWithOffset = offset => {
+  const currentTime = new Date();
+  const utcOffset = currentTime.getTimezoneOffset() / 60;
+  return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+};
+
+const Clock = ({ offset, location }) => {
+  const [time, setTime] = useState({ currentTime: getTimeWithOffset(offset) });
+  const { currentTime } = time;
+
+  useEffect(() => {
+    setInterval(() => {
+      setTime({
+        currentTime: getTimeWithOffset(offset),
+      });
+    }, 1000);
+  }, []);
+
+  return (
+    <div className="clock">
+      <div className="clock__location">{location}</div>
+      <div className="clock__time">{moment(currentTime).format('LTS')}</div>
+    </div>
+  );
+};
+
+export default Clock;
