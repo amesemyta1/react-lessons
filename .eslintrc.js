4818faf58151d066a0bfda56d9ee17549138eb98
amesemyta1 <amesemyta@gmail.com>
2022-10-16 20:33:09 +0300

    qsaw

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,40 @@
 module.exports = {
-  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  extends: ['airbnb-base', 'plugin:react/recommended', 'plugin:prettier/recommended'],
   env: {
     es6: true,
+    node: true,
     jest: true,
     browser: true,
   },
-  rules: {
-    'no-alert': 0,
-    'no-console': 0,
-    'no-constant-condition': 0,
-    'no-shadow': 0,
-    'no-restricted-globals': 0,
-    'no-unused-vars': 0,
-    'no-underscore-dangle': 0,
-    'func-names': 0,
-    'global-require': 0,
-    'import/extensions': 0,
-    'import/no-dynamic-require': 0,
-    'import/prefer-default-export': 0,
-    'prefer-template': 0,
-    'prettier/prettier': [
-      'warn',
-      {
-        endOfLine: 'auto',
-      },
-    ],
-  },
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2022,
+    ecmaVersion: 2021,
     sourceType: 'module',
+  },
+  rules: {
+    'import/extensions': 0,
+    'no-console': 0,
+    'no-alert': 0,
+    'react/prop-types': 0,
+    'import/no-unresolved': 0,
+    'global-require': 0,
+    'class-methods-use-this': 0,
+    'import/no-extraneous-dependencies': 0,
+    'arrow-body-style': 1,
+    'prefer-arrow-callback': 1,
   },
   settings: {
     'import/resolver': {
       node: {
         extensions: ['.js', '.jsx'],
+        moduleDirectory: ['node_modules', 'src/'],
       },
+    },
+    react: {
+      version: 'detect',
     },
   },
 };
